69e3f8a70f60d37827c56c46fbc600c11b433d17
Chubie Mirembe <71077124+ChubieMirembe@users.noreply.github.com>
2020-11-08 11:05:13 +0200

    Add files via upload

--- correlation.py
+++ correlation.py
@@ -1,79 +1,89 @@
-import pandas as pd     
+'''
+This is a simple program that shows the correlation between different forex pairs
+on different time frames.
+'''
+
+import pandas as pd
 
 df = pd.read_csv("correlation.csv")
 pair_list = df.pair1.unique()
 tf_list = list(df.columns[2::].values)
 
-def correlation1(): 
-    
+def correlation1():
     pair_1 = ''
-	pair_2 = ''
-	t_frame = ''
+    pair_2 = ''
+    t_frame = ''
 
     while pair_1 not in pair_list:
-            pair_1 = input("\nEnter the first pair: ").upper()
-               
-            if pair_1 not in pair_list:
-                 print("Input isn't valid! Try again")
-            else:
-                continue
-                
+        pair_1 = input("\nEnter the First pair: ").upper()
+
+        if pair_1 not in pair_list:
+            print("Input isn't valid! Try again")
+        else:
+            continue
+
     while pair_2 not in pair_list:
-            pair_2 = input("\nEnter the Second pair: ").upper()
-               
-            if pair_2 not in pair_list:
-                 print("Input isn't valid! Try again")
-            else:
-                continue
-                
+        pair_2 = input("\nEnter the Second pair: ").upper()
+
+        if pair_2 not in pair_list:
+            print("Input isn't valid! Try again")
+        else:
+            continue
+
     while t_frame not in tf_list:
-            t_frame = input("\nEnter the Time frame: ").lower()
-               
-            if t_frame not in tf_list:
-                 print("Input isn't valid! Try again")
-            else:
-                continue
+        t_frame = input("\nEnter the Time frame: ").lower()
+
+        if t_frame not in tf_list:
+            print("Input isn't valid! Try again")
+        else:
+            continue
 
     df1 = (df[df.pair1 == (pair_1)])
     df2 = (df1[df1.pair2 == (pair_2)])
-    
+
     for row in df2.index:
         corr = df2[t_frame][row]
         print(f"\nThe correlation betweeen {pair_1} and {pair_2} is {corr}")
 
 def correlation2():
 
-	pair_1 = ''
-	pair_2 = ''
-	t_frame = ''
+    pair_1 = ''
+    t_frame = ''
 
     while pair_1 not in pair_list:
-            pair_1 = input("\nEnter the first pair: ").upper()
-               
-            if pair_1 not in pair_list:
-                 print("Input isn't valid! Try again")
-            else:
-                continue
-                
-    while t_frame not in tf_list:
-            t_frame = input("\nEnter the Time frame: ").lower()
-               
-            if t_frame not in tf_list:
-                 print("Input isn't valid! Try again")
-            else:
-                continue
+        pair_1 = input("\nEnter the pair: ").upper()
 
-    df1 = (df[df.pair1 == (pair_1)])
+        if pair_1 not in pair_list:
+            print("Input isn't valid! Try again")
+        else:
+            continue
+
+    while t_frame not in tf_list:
+        t_frame = input("\nEnter the Time frame: ").lower()
+
+        if t_frame not in tf_list:
+            print("Input isn't valid! Try again")
+        else:
+            continue
+
+    df1 = (df[df.pair1 == (pair_1)]).sort_values(by=t_frame, ascending=False)
 
     for row in df1.index:
-        if df1[t_frame][row]> 80 or df1[t_frame][row]< -80:
+        if df1[t_frame][row] > 80 or df1[t_frame][row] < -80:
             print(df1['pair1'][row], df1['pair2'][row], df1[t_frame][row])
         else:
             continue
 
-number = int(input('How many pairs would you like to compare?: '))
+while True:
+    try:
+        number = int(input('How many pairs would you like to compare?: '))
+    except:
+        print("The input needs to be number between 2 and 10")
+        continue
+    else:
+        break
 
-if number <=2:
-    correlation1() 
+if number <= 2:
+    correlation1()
 else:
     correlation2()
